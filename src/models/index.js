6352943345e05037e8a974139885f72823c43db7
saurabh-kud <saurabh322001raj@gmail.com>
2024-05-04 18:57:48 +0530

    minor fixed

--- src/models/index.js
+++ src/models/index.js
@@ -9,17 +9,13 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 let sequelize;
-// console.log(config.database, config.username, config.password, config.host);
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+
+sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
 fs.readdirSync(__dirname)
   .filter((file) => {
     return (
